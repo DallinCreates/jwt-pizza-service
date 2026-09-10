de0a5e0d8139ba4335ef93fa0295b8bf0000c669
DallinCreates <dallink3@gmail.com>
2026-09-09 19:37:12 -0600

    adding registrion tests. 1 a valid user login in. 2 a invalid request missing a email.

--- tests/unit/routes/authRouter.test.js
+++ tests/unit/routes/authRouter.test.js
@@ -11,6 +11,27 @@ beforeAll(async () => {
   expectValidJwt(testUserAuthToken);
 });
 
+test('register', async () => {
+  const newUser = {
+    name: 'new diner',
+    email: Math.random().toString(36).substring(2, 12) + '@test.com',
+    password: 'b',
+  };
+  const registerRes = await request(app).post('/api/auth').send(newUser);
+  expect(registerRes.status).toBe(200);
+  expectValidJwt(registerRes.body.token);
+
+  const expectedUser = { ...newUser, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  expect(registerRes.body.user).toMatchObject(expectedUser);
+});
+
+test('register without email returns 400', async () => {
+  const registerRes = await request(app).post('/api/auth').send({ name: 'no email', password: 'x' });
+  expect(registerRes.status).toBe(400);
+  expect(registerRes.body.token).toBeUndefined();
+});
+
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
